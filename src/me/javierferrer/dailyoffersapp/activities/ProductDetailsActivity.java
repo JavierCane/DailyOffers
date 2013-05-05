2aa0922e1995f525c26dcfb03ed2881be97d575a
Javier Ferrer <javier.mailserio@gmail.com>
2013-05-05 22:24:11 +0200

    product details activity improved

--- src/me/javierferrer/dailyoffersapp/activities/ProductDetailsActivity.java
+++ src/me/javierferrer/dailyoffersapp/activities/ProductDetailsActivity.java
@@ -1,6 +1,7 @@
 package me.javierferrer.dailyoffersapp.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -9,6 +10,8 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 import me.javierferrer.dailyoffersapp.R;
 import me.javierferrer.dailyoffersapp.models.Product;
+
+import java.util.HashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,6 +26,7 @@ public final class ProductDetailsActivity extends SherlockActivity
 	private Product mProduct;
 	private ActionBar mActionBar;
 
+	private RelativeLayout mLayout;
 	private ImageView mIvProductImage;
 	private TextView mTvProductOfferPrice;
 	private TextView mTvProductPrice;
@@ -41,6 +45,25 @@ public final class ProductDetailsActivity extends SherlockActivity
 
 		initActionBar();
 
+		initViewVariables();
+
+		constructAttributesViews();
+	}
+
+	private void initActionBar()
+	{
+		mActionBar = getSupportActionBar();
+
+		mActionBar.setHomeButtonEnabled( true );
+		mActionBar.setDisplayHomeAsUpEnabled( true );
+		mActionBar.setTitle( mProduct.getName() );
+	}
+
+	private void initViewVariables()
+	{
+
+		mLayout = ( RelativeLayout ) findViewById( R.id.rl_product_details );
+
 		mIvProductImage = ( ImageView ) findViewById( R.id.iv_product_image );
 		mTvProductOfferPrice = ( TextView ) findViewById( R.id.tv_product_offer_price );
 		mTvProductPrice = ( TextView ) findViewById( R.id.tv_product_price );
@@ -50,7 +73,8 @@ public final class ProductDetailsActivity extends SherlockActivity
 				.setImageResource( ProductsListBaseActivity.getCategoriesImages().get( mProduct.getCategoryRoot() ) );
 		mTvProductOfferPrice
 				.setText( getResources().getString( R.string.offer_price ) + ": " + mProduct.getOfferPrice() + "€" );
-		mTvProductPrice.setText( getResources().getString( R.string.regular_price ) + ": " + mProduct.getPrice() + "€" );
+		mTvProductPrice
+				.setText( getResources().getString( R.string.regular_price ) + ": " + mProduct.getPrice() + "€" );
 
 		mCbFavorited.setTag( mProduct );
 
@@ -71,24 +95,56 @@ public final class ProductDetailsActivity extends SherlockActivity
 
 		mCbFavorited.setChecked( mProduct.isFavorited() );
 
-		final Button btnBuyProduct = ( Button ) findViewById( R.id.btn_buy );
-		btnBuyProduct.setOnClickListener( new View.OnClickListener()
+		mBtnBuyProduct = ( Button ) findViewById( R.id.btn_buy );
+
+		// Buy button onClick callback -> Init webview activity
+		mBtnBuyProduct.setOnClickListener( new View.OnClickListener()
 		{
 
 			public void onClick( View view )
 			{
-				// Perform action on click
+				Intent intent = new Intent( ProductDetailsActivity.this, WebViewActivity.class );
+
+				Bundle bundle = new Bundle();
+				bundle.putString( "url", mProduct.getBuyUrl() );
+				intent.putExtras( bundle );
+
+				startActivity( intent );
 			}
 		} );
 	}
 
-	private void initActionBar()
+	private void constructAttributesViews()
 	{
-		mActionBar = getSupportActionBar();
+		int attributeIterator = 555;
 
-		mActionBar.setHomeButtonEnabled( true );
-		mActionBar.setDisplayHomeAsUpEnabled( true );
-		mActionBar.setTitle( mProduct.getName() );
+		for ( HashMap<String, String> attribute : mProduct.getAttributes() )
+		{
+			TextView tv = new TextView( getApplicationContext() );
+			tv.setId( attributeIterator );
+			tv.setText( attribute.get( "key" ) + ": " + attribute.get( "value" ) );
+			tv.setTextSize( 15 );
+			tv.setTextColor( Color.BLACK );
+
+			RelativeLayout.LayoutParams params =
+					new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT,
+							RelativeLayout.LayoutParams.WRAP_CONTENT );
+			params.addRule( RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE );
+
+			if ( attributeIterator == 555 )
+			{
+				params.addRule( RelativeLayout.BELOW, R.id.tv_basic_data );
+				params.setMargins( 30, 15, 0, 0 ); // Left, top, right, bottom.
+			}
+			else
+			{
+				params.setMargins( 30, 5, 0, 0 ); // Left, top, right, bottom.
+				params.addRule( RelativeLayout.BELOW, attributeIterator - 1 );
+			}
+
+			mLayout.addView( tv, params );
+			attributeIterator++;
+		}
 	}
 
 	@Override
